db6a051af99114d79c053b2f977f95dc987faa9d
hant <thuha291205@gmail.com>
2026-06-09 00:22:36 +0700

    fix x modal in minicart

--- magento/app/code/TeknTek/CatalogTweaks/Observer/RequireLoginForCustomerActions.php
+++ magento/app/code/TeknTek/CatalogTweaks/Observer/RequireLoginForCustomerActions.php
@@ -21,9 +21,11 @@ class RequireLoginForCustomerActions implements ObserverInterface
      */
     private array $protectedActions = [
         // 'checkout_cart_add',
-        'review_product_post',
-        'tekntek_suggest_review_submit',
+        // 'review_product_post',
+        // 'tekntek_suggest_review_submit',
+        // 'wishlist_index_add',
         'wishlist_index_add',
+        'catalog_product_compare_add',
     ];
 
     public function __construct(
